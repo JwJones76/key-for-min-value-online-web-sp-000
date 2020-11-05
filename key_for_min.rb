6efdf6730280096dfbba5334c1c11df0a860a6dd
Jacob W Jones <jacobwadejones76@gmail.com>
2020-11-05 19:16:52 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,11 +1,11 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 
-def key_of_min_value(hash_of_names) #accepts hash where key/value pairs are names/integers
+def key_for_min_value(hash_of_names) 
   if hash_of_names.empty?
     nil
   else
-    array_of_names = hash_of_names.to_a.flatten #turn hash to AOA, then A
+    array_of_names = hash_of_names.to_a.flatten 
     integer_array = []
     array_of_names.each do |element|
       if element.is_a? Integer
