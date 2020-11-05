ee4f2219d30d6c979df23afca5fec6e904d47bd9
Jacob W Jones <jacobwadejones76@gmail.com>
2020-11-05 19:13:34 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,7 +1,23 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 
-
+def key_of_min_value(hash_of_names) #accepts hash where key/value pairs are names/integers
+  if hash_of_names.empty?
+    nil
+  else
+    array_of_names = hash_of_names.to_a.flatten #turn hash to AOA, then A
+    integer_array = []
+    array_of_names.each do |element|
+      if element.is_a? Integer
+        integer_array << element #shovel values/integers into integer array
+      end
+    end
+    max_integer_array = integer_array.max(integer_array.length - 1) #create max array of highest values except one, the minimum value
+    min_integer = integer_array.difference(max_integer_array).join.to_i #isolate minimum integer by taking difference between integer array and max integer array, then turn to integer
+    min_integer_index = array_of_names.index(min_integer) #find index of min value
+    array_of_names.fetch(min_integer_index - 1) #return key of minimum value which is positioned one index before min integer
+  end
+en
 
     
   
