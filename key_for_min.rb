af3f1082a9a0d6f362796aace62b9d00401d0905
Jacob W Jones <jacobwadejones76@gmail.com>
2020-11-05 19:09:38 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,12 +2,11 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
+  if name_hash.empty?
+    nil
+  else
   name_hash.collect do |name, age|
-    if name_hash.empty?
-      nil
-    end
-  end
-end
+  
   
  
 
